aa0d52654d02c37d1e25de2aa2de24e969381eea
zengogh <zencomcn@live.com>
2022-12-09 14:43:29 +0800

    Update index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 这是一个Typecho主题，适合制作资源下载类型网站
+ *
+ * @package 模高库-MOGOCO
+ * @author Gogh Studio
+ * @version 0.022.11.28
+ * @link http://mogoco.ml
+ */
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+$this->need('header.php'); ?>
+
+<div class="sidebar-left">
+<?php $this->need('/Components/accordion.php'); ?>
+</div>
+<div class="website-body">
+
+</div>

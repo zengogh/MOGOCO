4c2403dc63a65d8939d0f0c2573dd70c7048618b
zengogh <zencomcn@live.com>
2022-12-16 10:55:58 +0800

    更新 right-sidebar.php

--- Components/right-sidebar.php
+++ Components/right-sidebar.php
@@ -15,5 +15,16 @@
         <li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
         <li itemprop="keywords" class="tags"><?php _e('标签: '); ?><?php $this->tags(', ', true, 'none'); ?></li>
     </ul>
-
+<!--输出标签云 -->
+<?php $this->widget('Widget_Metas_Tag_Cloud', 'sort=mid&ignoreZeroCount=1&desc=0&limit=10')->to($tags); ?>
+<?php if($tags->have()): ?>
+    <h4 class="tags-title"><i class="bi bi-tags-fill"></i> <?php _e('热门搜索'); ?></h4>
+<ul class="tags-list">
+<?php while ($tags->next()): ?>
+    <li><a href="<?php $tags->permalink(); ?>" rel="tag" class="size-<?php $tags->split(5, 10, 20, 30); ?>" title="<?php $tags->count(); ?> 个话题"><?php $tags->name(); ?></a></li>
+<?php endwhile; ?>
+<?php else: ?>
+    <li><?php _e('没有任何标签'); ?></li>
+<?php endif; ?>
+</ul>
 </div>
